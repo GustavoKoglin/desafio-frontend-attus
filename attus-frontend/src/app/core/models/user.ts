539f570a81b61b58aafea265a701527da3d0f7f3
Gustavo Koglin <contato@devgustavokoglin.com.br>
2026-05-25 23:20:34 -0400

    Fix: Adiciona propriedades role, type e password na interface User para compilação

--- attus-frontend/src/app/core/models/user.ts
+++ attus-frontend/src/app/core/models/user.ts
@@ -1,8 +1,11 @@
 export interface User {
-  id: string;
+  id?: string;
   name: string;
   email: string;
   cpf: string;
   phone: string;
   phoneType: 'celular' | 'residencial' | 'trabalho' | 'comercial';
+  role?: string;
+  type?: 'App' | 'Platform';
+  password?: string;
 }
